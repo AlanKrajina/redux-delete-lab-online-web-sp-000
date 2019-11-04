f08893757e2b74afcc7ef9ce38f2535a15d5c8f8
Alan Krajina <alankrajina88@gmail.com>
2019-11-04 17:07:12 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -14,13 +14,12 @@ export default function manageBand(state = {
       return { ...state, bands: [...state.bands, band] }
 
     case 'DELETE_BAND':
- 
+
       const bands = state.bands.filter(band => band.id !== action.id);
       return { bands };
        // id check sa action data id
- 
+
     default:
       return state;
   }
 };
-
